e40e4fd1066183fc4fc1cf8371d37647a5613ed6
Kacper <kacper.dudczak19@gmail.com>
2022-03-16 21:39:00 +0100

    Update
    Update

---
+++ Ticket_Generator/Ticket_Generator.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	date := "Date"
+	space_line := "Space line"
+	days := "Days"
+	type1 := "Type"
+	price := "Price"
+
+	fmt.Printf("%-15v %-15v %-15v %-15v $ %-15v ", date, space_line, days, type1, price)
+
+}
